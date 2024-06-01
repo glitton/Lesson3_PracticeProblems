227bf1f375b40726932a364d3975c25ee75a2c10
Generosa Litton <glcodeworks@gmail.com>
2024-06-01 05:28:15 -0500

    through question 5

--- 01_practiceproblems_easy1/easy2.js
+++ 01_practiceproblems_easy1/easy2.js
@@ -3,17 +3,35 @@ let advice =
 // console.log(advice.replace("important", "urgent"));
 // console.log(advice.replaceAll("important", "urgent"));
 
-let numbers = [1, 2, 3, 4, 5];
+// let numbers = [1, 2, 3, 4, 5];
 // console.log(-(numbers.length - 1));
 // let reversedArray = numbers.slice().reverse();
 
-let sortedReversedArray = [...numbers].sort((num1, num2) => num2 - num1);
+// let sortedReversedArray = [...numbers].sort((num1, num2) => num2 - num1);
 // console.log(sortedReversedArray);
 
-let reversedArray = [];
+// let reversedArray = [];
 
-numbers.forEach((number) => {
-  reversedArray.unshift(number);
-});
+// numbers.forEach((number) => {
+//   reversedArray.unshift(number);
+// });
 
-console.log(reversedArray);
+// console.log(reversedArray);
+
+// let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+
+// let number1 = 8; // false
+// let number2 = 95; // true
+
+// console.log(numbers.includes(number1));
+// console.log(numbers.includes(number2));
+
+let famousWords = "seven years ago...";
+let beginningFamousWords = "Four score and ";
+// let final = `${beginningFamousWords}${famousWords}`;
+let final = beginningFamousWords.concat(famousWords);
+console.log(final);
+
+let numbers = [1, 2, 3, 4, 5];
+numbers.splice(1, 1);
+console.log(numbers);
