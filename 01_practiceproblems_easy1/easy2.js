5e7e9d30b8184252bafcc1f99067c63c89ef4e4f
Generosa Litton <glcodeworks@gmail.com>
2024-06-01 05:11:47 -0500

    finished questions 1 and 2

--- 01_practiceproblems_easy1/easy2.js
+++ 01_practiceproblems_easy1/easy2.js
@@ -1,4 +1,19 @@
 let advice =
   "Few things in life are as important as house training your important pet dinosaur.";
 // console.log(advice.replace("important", "urgent"));
-console.log(advice.replaceAll("important", "urgent"));
+// console.log(advice.replaceAll("important", "urgent"));
+
+let numbers = [1, 2, 3, 4, 5];
+// console.log(-(numbers.length - 1));
+// let reversedArray = numbers.slice().reverse();
+
+let sortedReversedArray = [...numbers].sort((num1, num2) => num2 - num1);
+// console.log(sortedReversedArray);
+
+let reversedArray = [];
+
+numbers.forEach((number) => {
+  reversedArray.unshift(number);
+});
+
+console.log(reversedArray);
