e95389bdee038a84276a9456ab49b8acea16659e
Generosa Litton <glcodeworks@gmail.com>
2024-06-05 16:25:33 -0500

    finished hard1

--- 01_practiceproblems_easy1/hard1.js
+++ 01_practiceproblems_easy1/hard1.js
@@ -29,11 +29,11 @@ numArray.push(2);
 
 // Question 3
 // A
-// function messWithVars(one, two, three) {
-//   one = two;
-//   two = three;
-//   three = one;
-// }
+function messWithVars(one, two, three) {
+  one = two;
+  two = three;
+  three = one;
+}
 
 // let one = ["one"];
 // let two = ["two"];
@@ -47,11 +47,11 @@ numArray.push(2);
 // console.log(`three is: ${three}`);
 
 // B
-// function messWithVars(one, two, three) {
-//   one = ["two"];
-//   two = ["three"];
-//   three = ["one"];
-// }
+function messWithVars(one, two, three) {
+  one = ["two"];
+  two = ["three"];
+  three = ["one"];
+}
 
 // let one = ["one"];
 // let two = ["two"];
@@ -70,11 +70,11 @@ function messWithVars(one, two, three) {
   three.splice(0, 1, "one");
 }
 
-let one = ["one"];
-let two = ["two"];
-let three = ["three"];
+// let one = ["one"];
+// let two = ["two"];
+// let three = ["three"];
 
-messWithVars(one, two, three);
+// messWithVars(one, two, three);
 
 // console.log(`one is: ${one}`);
 // console.log(`two is: ${two}`);
@@ -83,18 +83,63 @@ messWithVars(one, two, three);
 // Question 4: identify all of the variables, primitive values, and objects
 
 function boo(scare) {
-  let myBoo = scare.toUpperCase() + "!!!";
-  console.log(myBoo);
+  let myBoo = scare.toUpperCase() + "!!!"; //3 string
+  console.log(myBoo); //1 string
 }
 
 const halloweenCollection = {
+  //6 strings
   greet: "Happy Halloween",
   scare: "Boo",
   wish: "May all your pumpkins be glowing",
 };
-
-let myBoo = boo(halloweenCollection["greet"]);
+//function returns undefined which is another primitive, 1
+// let myBoo = boo(halloweenCollection["greet"]); // passing string by reference and greet string, 2
 
 //Variables: function boo, scare parameter, myBoo, halloweenCollection, let myBoo => 5
-//Primitive values: strings: "scare.toUpperCase + !!!", object "greet": "Happy Halloween", "Boo", "May all ..." => 6
+//Primitive values:
 //Objects: {greet: ...}, halloweenCollection["greet"] => 2
+
+// Question 5
+// function isDotSeparatedIpAddress(inputString) {
+//   let dotSeparatedWords = inputString.split(".");
+//   console.log(dotSeparatedWords);
+//   while (dotSeparatedWords.length > 0) {
+//     let word = dotSeparatedWords.pop();
+//     if (!isAnIpNumber(word)) {
+//       break;
+//     }
+//   }
+
+//   return true;
+// }
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  // console.log(dotSeparatedWords);
+  if (dotSeparatedWords.length !== 4) {
+    console.log("Invalid input");
+    return false;
+  }
+
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
+
+  return false;
+}
+
+// console.log(isAnIpNumber("hello.2.45"));
+console.log(isDotSeparatedIpAddress("hello.2.45"));
