bc51a428873270813fc409feadc6fa2b829eb51e
Generosa Litton <glcodeworks@gmail.com>
2024-06-04 20:23:15 -0500

    answered through question 4

--- 01_practiceproblems_easy1/hard1.js
+++ 01_practiceproblems_easy1/hard1.js
@@ -21,18 +21,53 @@ let object = { first: [1] };
 let numArray = object["first"];
 numArray.push(2);
 
-console.log(numArray); //  => "[1, 2]"
-console.log(object);
+// console.log(numArray); //  => "[1, 2]"
+// console.log(object);
 // {
 //   first: [1, 2];
 // }
 
 // Question 3
 // A
+// function messWithVars(one, two, three) {
+//   one = two;
+//   two = three;
+//   three = one;
+// }
+
+// let one = ["one"];
+// let two = ["two"];
+// let three = ["three"];
+
+// messWithVars(one, two, three); // once invoked, vars take on new values
+
+// console.log(`one is: ${one}`);
+// // console.log(typeof one);
+// console.log(`two is: ${two}`);
+// console.log(`three is: ${three}`);
+
+// B
+// function messWithVars(one, two, three) {
+//   one = ["two"];
+//   two = ["three"];
+//   three = ["one"];
+// }
+
+// let one = ["one"];
+// let two = ["two"];
+// let three = ["three"];
+
+// messWithVars(one, two, three);
+
+// console.log(`one is: ${one}`);
+// console.log(`two is: ${two}`);
+// console.log(`three is: ${three}`);
+
+// C
 function messWithVars(one, two, three) {
-  one = two;
-  two = three;
-  three = one;
+  one.splice(0, 1, "two");
+  two.splice(0, 1, "three");
+  three.splice(0, 1, "one");
 }
 
 let one = ["one"];
@@ -41,7 +76,25 @@ let three = ["three"];
 
 messWithVars(one, two, three);
 
-console.log(`one is: ${one}`);
-console.log(typeof one);
-console.log(`two is: ${two}`);
-console.log(`three is: ${three}`);
+// console.log(`one is: ${one}`);
+// console.log(`two is: ${two}`);
+// console.log(`three is: ${three}`);
+
+// Question 4: identify all of the variables, primitive values, and objects
+
+function boo(scare) {
+  let myBoo = scare.toUpperCase() + "!!!";
+  console.log(myBoo);
+}
+
+const halloweenCollection = {
+  greet: "Happy Halloween",
+  scare: "Boo",
+  wish: "May all your pumpkins be glowing",
+};
+
+let myBoo = boo(halloweenCollection["greet"]);
+
+//Variables: scare, myBoo, halloweenCollection, let myBoo => 4
+//Primitive values: strings: "scare.toUpperCase + !!!", "Happy Halloween", "Boo", "May all ..." => 4
+//Objects: {greet: ...}, halloweenCollection["greet"]
