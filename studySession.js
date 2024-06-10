70f19b73180cf4ab51ae0b9e9e64e157542d04ae
Generosa Litton <glcodeworks@gmail.com>
2024-06-10 18:57:17 -0500

    more array exercises

--- studySession.js
+++ studySession.js
@@ -148,5 +148,24 @@ function oddLengthsArray(strings) {
   }, []);
 }
 
-let arr = ["a", "abcd", "abcde", "abc", "ab"];
-console.log(oddLengthsArray(arr));
+// let arr = ["a", "abcd", "abcde", "abc", "ab"];
+// console.log(oddLengthsArray(arr));
+
+// function numberThree(arr) {
+//   return arr.includes(3);
+// }
+
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+// console.log(numberThree(numbers3));
+
+let arr = [
+  ["hello", "world"],
+  ["example", "mem", null, 6, 88],
+  [4, 8, 12],
+];
+arr[1][3] = 606;
+
+console.log(arr);
