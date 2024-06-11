7045e3ddc55b8879b936eef66bcb1e5039abe747
Generosa Litton <glcodeworks@gmail.com>
2024-06-11 17:23:26 -0500

    JS109 snippets for objects

--- JS109review.js
+++ JS109review.js
@@ -3,11 +3,16 @@
 // a = 7;
 // console.log(b);
 
-let num = 1;
+// let num = 1;
 
-while (num < 3) {
-  let num = 5;
-  num += 1;
-}
+// while (num < 3) {
+//   let num = 5;
+//   num += 1;
+// }
 
-console.log(num);
+// console.log(num);
+
+let word = "hello";
+let newWord = word.replace("h", "j");
+
+console.log(newWord); // 'jello', old word var doesn't change
