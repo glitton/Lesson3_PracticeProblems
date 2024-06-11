4c41c087ac828412f9da163bb5e42fb4c8cb6089
Generosa Litton <glcodeworks@gmail.com>
2024-06-11 17:18:33 -0500

    JS109 code snippet

--- JS109review.js
+++ JS109review.js
@@ -1,4 +1,13 @@
-let a = 4;
-let b = a;
-a = 7;
-console.log(b);
+// let a = 4;
+// let b = a;
+// a = 7;
+// console.log(b);
+
+let num = 1;
+
+while (num < 3) {
+  let num = 5;
+  num += 1;
+}
+
+console.log(num);
